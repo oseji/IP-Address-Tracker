c3698989a785c1d113c836aa1551463ae4a1d4c9
oseji <jiade1233@gmail.com>
2023-11-25 14:48:11 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { useState, useEffect, useRef } from "react";
+
+import { motion } from "framer-motion";
 import {
   MapContainer,
   TileLayer,
@@ -9,8 +11,6 @@ import {
 } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import iconArrow from "./assets/icon-arrow.svg";
-import mobileBg from "./assets/pattern-bg-mobile.png";
-import desktopBg from "./assets/pattern-bg-desktop.png";
 
 function App() {
   const [position, setPosition] = useState([0, 0]);
@@ -28,52 +28,63 @@ function App() {
 
   const [ipType, setIpType] = useState("loading");
 
-  // const [apiLink, setApiLink] = useState(
-  //   `https://geo.ipify.org/api/v2/country,city?apiKey=at_xFRAXtrPbpaoVAwKOkSYAXyH9bveA&ipAddress=${ipAddress}`
-  // );
-
   const [apiLink, setApiLink] = useState(
-    `http://api.ipstack.com/${ipAddress}?access_key=d0caedd7945c2290f5b95ed0504d5419`
+    `https://geo.ipify.org/api/v2/country,city?apiKey=at_xFRAXtrPbpaoVAwKOkSYAXyH9bveA&ipAddress=8.8.8.8`
   );
 
   const [error, setError] = useState(null);
 
-  //fetch IP address
-  useEffect(() => {
-    const fetchIpAddress = async () => {
-      try {
-        const response = await fetch("https://api.ipify.org?format=json");
-        const data = await response.json();
-        setIpAddress(data.ip);
-        setApiLink(
-          `http://api.ipstack.com/${data.ip}?access_key=d0caedd7945c2290f5b95ed0504d5419`
-        );
-      } catch (error) {
-        console.error("Error fetching IP address: ", error);
-      }
-    };
-
-    fetchIpAddress();
-  }, []);
-
+  //GET DEVICE LOCATION FOR THE MAPS INITIAL LOAD
   const getLocation = (e) => {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
         (e) => {
           console.log(e);
           setPosition([e.coords.latitude, e.coords.longitude]);
+          //setPosition(ipPosition);
           //console.log(position);
           setLoad(false);
         },
         (err) => {
           setError(err.message);
-          alert(error);
+          //alert(error);
         }
       );
     } else {
       setError("Geolocation is currently not supported by this device");
     }
   };
+
+  //fetch data and get location when ipAddress changes
+  useEffect(() => {
+    getLocation();
+    //fetchData();
+  }, []);
+
+  //FETCH IP ADDRESS WHENEVER DEVICE LOCATION CHANGES
+  useEffect(() => {
+    const fetchIpAddress = async () => {
+      try {
+        //fetching device IP
+        const response = await fetch("https://api.ipify.org?format=json");
+        const data = await response.json();
+        console.log(data);
+
+        setIpAddress(data.ip);
+
+        setApiLink(
+          `https://geo.ipify.org/api/v2/country,city?apiKey=at_xFRAXtrPbpaoVAwKOkSYAXyH9bveA&ipAddress=${data.ip}`
+        );
+
+        coords.load(apiLink);
+        //setIpPosition([data.latitude, data.longitude]);
+      } catch (error) {
+        console.error("Error fetching IP address: ", error);
+      }
+    };
+
+    fetchIpAddress();
+  }, [position]);
 
   const fetchData = async () => {
     setApiLoading(true);
@@ -88,12 +99,12 @@ function App() {
       setData(data);
       console.log(data);
 
-      setCity(data.city);
-      setCountry(data.country_name);
-      setIpType(data.type);
-      setIpPosition([data.latitude, data.longitude]);
+      setCity(data.location.city);
+      setCountry(data.location.country);
+      setIpType(data.isp);
+      setIpPosition([data.location.lat, data.location.lng]);
 
-      console.log(position, ipPosition);
+      console.log(position, ipPosition, apiLink);
     } catch (error) {
       setError(error);
       // if (error) {
@@ -104,28 +115,44 @@ function App() {
     }
   };
 
-  //fetch data and get location when ipAddress changes
-  useEffect(() => {
-    getLocation();
-    //fetchData();
-  }, [ipAddress]);
-
-  useEffect(() => {
-    fetchData();
-  }, [position]);
-
   const submitBtn = (e) => {
     e.preventDefault();
     console.log(inputIP, position);
+
+    setIpAddress(inputIP);
 
     fetchData();
 
     setInputIP("");
   };
 
+  // useEffect(() => {
+  //   fetchData();
+  // }, [position, apiLink]);
+
+  const loadingVariants = {
+    animation: {
+      x: [-100, 100],
+      // y: [100, -70, 100],
+
+      transition: {
+        x: {
+          repeat: Infinity,
+          yoyo: Infinity,
+          duration: 1,
+        },
+        y: {
+          repeat: Infinity,
+          yoyo: Infinity,
+          duration: 1,
+        },
+      },
+    },
+  };
+
   return (
     <div className="app">
-      <header>
+      <header className=" bg-[url('./assets/pattern-bg-mobile.png')] lg:bg-[('./assets/pattern-bg-desktop.png')]">
         <h1 className="heading">IP Address Tracker</h1>
 
         <div className="searchSection">
@@ -136,6 +163,9 @@ function App() {
             value={inputIP}
             onChange={(e) => {
               setInputIP(e.target.value);
+              setApiLink(
+                `https://geo.ipify.org/api/v2/country,city?apiKey=at_xFRAXtrPbpaoVAwKOkSYAXyH9bveA&ipAddress=${e.target.value}`
+              );
               console.log(inputIP);
             }}
           />
@@ -173,15 +203,25 @@ function App() {
         {/* if location data is still loading */}
         {load && (
           <div className="loadingScreen">
-            <h1 className="text-center text-xl lg:text-4xl mt-32 lg:mt-52 font-semibold">
-              Loading please wait....
-            </h1>
+            {/* <div className="flex flex-row items-center justify-center gap-5">
+              <h1 className="text-center text-xl lg:text-4xl mt-32 lg:mt-52 font-semibold">
+                Loading please wait....
+              </h1>
+
+             
+            </div> */}
+
+            <motion.div
+              variants={loadingVariants}
+              animate="animation"
+              className="loader"
+            ></motion.div>
           </div>
         )}
 
         {/* if location data is done loading */}
         {!load && (
-          <MapContainer center={position} zoom={8} scrollWheelZoom={false}>
+          <MapContainer center={position} zoom={12} scrollWheelZoom={false}>
             <TileLayer
               attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
